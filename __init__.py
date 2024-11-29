0523f7371f34caca6e8b2a08bf74309d974fc16d
Clinton Nguyen <111333589+Notnilc2107@users.noreply.github.com>
2024-11-29 13:51:53 +1000

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ from gym.envs.registration import register
 logger = logging.getLogger(__name__)
 
 register(
-    id='Inventory-v0',
+    id='gym-inventory/Inventory-v0',
     entry_point='gym_inventory.envs:InventoryEnv',
     max_episode_steps=1000,
     reward_threshold=1.0,
